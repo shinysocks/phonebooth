cbb4f7a72e271bb57479115fbca9391c6149e43a
Noah Dinan <noah@shinysocks.net>
2025-03-18 11:32:43 -0500

    working before file changes

--- phonebooth.cpp
+++ phonebooth.cpp
@@ -8,22 +8,9 @@
     https://docs.google.com/document/d/1XouO8o9H_l_IDd2_Z0JVcL264ybqdvaisL009b8jgXM
  */
 
-
-// refactor todo:
-//      move PA stuff to class
-//      move average to func
-//      fix error handling
-//          [
-//              throw an exception object
-//              in main, try and then catch exception object and handle
-//          ]
-//      more file operations to func
-//      clean up main while loop
-
-
 #include <portaudio.h>
 #include <iostream>
-/*#include <fstream>*/
+#include <fstream>
 #include <vector>
 
 using namespace std;
@@ -39,56 +26,19 @@ PaStream* stream;
 int end(PaError);
 double average_buffer_level(float[]);
 void play(vector<float> &phrase);
+PaError initialize();
 
 int main() {
-    PaStreamParameters* inputParams = new PaStreamParameters;
-    PaStreamParameters* outputParams = new PaStreamParameters;
-    PaError err;
-
-    err = Pa_Initialize();
+    PaError err = initialize();
     if (err != paNoError) end(err);
-
-    // fill input and output parameters
-    inputParams->device = Pa_GetDefaultInputDevice();
-    inputParams->channelCount = NUM_CHANNELS;
-    inputParams->sampleFormat = PA_SAMPLE_TYPE;
-    inputParams->suggestedLatency = Pa_GetDeviceInfo(inputParams->device)->defaultHighInputLatency;
-    inputParams->hostApiSpecificStreamInfo = NULL;
-
-    outputParams->device = Pa_GetDefaultOutputDevice();
-    outputParams->channelCount = NUM_CHANNELS;
-    outputParams->sampleFormat = PA_SAMPLE_TYPE;
-    outputParams->suggestedLatency = Pa_GetDeviceInfo(outputParams->device)->defaultHighOutputLatency;
-    outputParams->hostApiSpecificStreamInfo = NULL;
-
-    // opens input and output stream
-    err = Pa_OpenStream(
-            &stream,
-            inputParams,
-            outputParams,
-            SAMPLE_RATE,
-            FRAMES_PER_BUFFER,
-            paClipOff,
-            NULL,
-            NULL);
-    if (err != paNoError) end(err);
-
-    // start stream
-    err = Pa_StartStream(stream);
-    if (err != paNoError) end(err);
-
-    Pa_Sleep(1000);
 
     float prev_buffer[FRAMES_PER_BUFFER];
-
     while (true) {
         float buffer[FRAMES_PER_BUFFER];
         Pa_ReadStream(stream, buffer, FRAMES_PER_BUFFER);
 
         if (average_buffer_level(buffer) > AMBIENT_THRESHOLD) {
-            cout << "starting recording!" << endl;
-            // record buffer and start recording and waiting for silence
-
+            cout << "started recording" << endl;
             int silence_counter = 0;
             vector<float> phrase;
 
@@ -111,7 +61,7 @@ int main() {
             }
 
             // is phrase vector length long enough?
-            // if (phrase.size() > 3 seconds) {}
+            /*if (phrase.size() > 3 seconds) {}*/
 
             play(phrase);
         } else {
@@ -138,11 +88,51 @@ int main() {
     return end(err);
 }
 
+PaError initialize() {
+    PaStreamParameters* inputParams = new PaStreamParameters;
+    PaStreamParameters* outputParams = new PaStreamParameters;
+    PaError err;
+
+    err = Pa_Initialize();
+
+    // fill input and output parameters
+    inputParams->device = Pa_GetDefaultInputDevice();
+    inputParams->channelCount = NUM_CHANNELS;
+    inputParams->sampleFormat = PA_SAMPLE_TYPE;
+    inputParams->suggestedLatency = Pa_GetDeviceInfo(inputParams->device)->defaultHighInputLatency;
+    inputParams->hostApiSpecificStreamInfo = NULL;
+
+    outputParams->device = Pa_GetDefaultOutputDevice();
+    outputParams->channelCount = NUM_CHANNELS;
+    outputParams->sampleFormat = PA_SAMPLE_TYPE;
+    outputParams->suggestedLatency = Pa_GetDeviceInfo(outputParams->device)->defaultHighOutputLatency;
+    outputParams->hostApiSpecificStreamInfo = NULL;
+
+    // opens input and output stream
+    err = Pa_OpenStream(
+            &stream,
+            inputParams,
+            outputParams,
+            SAMPLE_RATE,
+            FRAMES_PER_BUFFER,
+            paClipOff,
+            NULL,
+            NULL);
+
+    // start stream
+    err = Pa_StartStream(stream);
+
+    return err;
+}
+
 void play(vector<float> &phrase) {
-    for (unsigned int i = 0; i < (phrase.size() / FRAMES_PER_BUFFER) - (4 * SAMPLE_RATE / FRAMES_PER_BUFFER); i++) { // don't play the a second at the end
+    int playback_duration = (phrase.size() / FRAMES_PER_BUFFER) - (4 * SAMPLE_RATE / FRAMES_PER_BUFFER);
+
+    for (int i = 0; i < playback_duration; i++) { // don't play 4 seconds at the end
         Pa_WriteStream(stream, &phrase[i * FRAMES_PER_BUFFER], FRAMES_PER_BUFFER);
     }
-    cout << "finished playing the recording" << endl;
+
+    cout << "played" << endl;
     phrase.clear();
 }
 
